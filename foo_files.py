4517b1e2d9dec497412ff2bab467bbff8633e47a
Mike Levin <miklevin@gmail.com>
2026-09-15 15:34:27 -0400

    chore(router): set AI-edit blast boundary (foo_files.py)

--- foo_files.py
+++ foo_files.py
@@ -3038,7 +3038,6 @@ foo_files.py
 # remotes/honeybot/queries/hydration_selftest.sql  # [1,204 tokens | 4,235 bytes]
 # remotes/honeybot/queries/intel_markdown_affinity.sql  # [434 tokens | 1,686 bytes]
 # remotes/honeybot/queries/ua_variants.sql  # [645 tokens | 2,535 bytes]
-# remotes/honeybot/www/npvg.org/index.html  # [331 tokens | 1,129 bytes]
 # scripts/articles/Notebooks/Shared/README.md  # [136 tokens | 605 bytes]
 # scripts/articles/build_hierarchy.py  # [2,460 tokens | 10,361 bytes]
 # scripts/articles/build_navgraph.py  # [2,119 tokens | 9,029 bytes]
